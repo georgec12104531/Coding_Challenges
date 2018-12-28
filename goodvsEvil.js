784cdea07febbb324834b78a48329d429bb3233a
George Charles Chang <31757304+georgec12104531@users.noreply.github.com>
2018-12-28 14:19:45 -0800

    Update goodVsEvil.js

--- goodvsEvil.js
+++ goodvsEvil.js
@@ -45,4 +45,20 @@ function goodVsEvil(good, evil) {
   }
 }
 
+function goodVsEvil(good, evil) {  
+  var getWorth = function( side, worth ) {
+    
+    return side.split(' ').reduce( function(result, value, index) { 
+      return result + (worth[index] * value);
+    }, 0);
+  }
+
+  var result = getWorth( good, [1,2,3,3,4,10] ) - getWorth( evil, [1,2,2,2,3,5,10] );
+
+  return result > 0 ? "Battle Result: Good triumphs over Evil" :
+         result < 0 ? "Battle Result: Evil eradicates all trace of Good" :
+                      "Battle Result: No victor on this battle field";
+}
+
+
 
