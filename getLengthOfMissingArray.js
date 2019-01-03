6ce54ef4afd2700c492c6d6a8c541626f88b86bd
George Charles Chang <31757304+georgec12104531@users.noreply.github.com>
2019-01-02 21:46:30 -0800

    Create getLengthOfMissingArray.js

---
+++ getLengthOfMissingArray.js
@@ -0,0 +1,13 @@
+const getLengthOfMissingArray = (arrays) => {
+  const lengths = (arrays || [])
+    .map(arr => arr ? arr.length : 0)
+    .sort((a, b) => a - b)
+      
+  if (lengths.includes(0)) return 0
+
+  for (let i = 0; i < lengths.length - 1; i++) {
+    if (lengths[i + 1] !== lengths[i] + 1) return lengths[i] + 1
+  }
+
+  return 0;
+}
