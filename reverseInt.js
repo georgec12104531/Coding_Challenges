8571e9298491ee616e66ccb91ff8dd67b833169b
George Charles Chang <31757304+georgec12104531@users.noreply.github.com>
2019-01-07 16:35:03 -0800

    Update reverseInt.js

--- reverseInt.js
+++ reverseInt.js
@@ -7,3 +7,17 @@ const reverseInt = (num) => {
 
   return neg ? Number(reverseNum.slice(0, reverseNum.length-1)) * -1 : Number(reverseNum)
 }
+
+const reverseInt = (int) => {
+  let neg = Math.abs(int) === (int * -1)
+  let num = Math.abs(int)
+  let rev = 0
+  
+  while (num !== 0) {
+    let pop = num % 10
+    num = Math.floor(num / 10)
+    rev = rev * 10 + pop
+  }
+
+  return neg ? -1 * rev : rev
+}
