247cea5ce8fc52a8bc0f540174cdbb6c01cd3afb
George Charles Chang <31757304+georgec12104531@users.noreply.github.com>
2019-01-18 14:51:47 -0800

    Update HammingDistance.js

--- hammingDistance.js
+++ hammingDistance.js
@@ -1,52 +1,8 @@
-  const bitHash = {
-  0: '0000',
-  1: '0001',
-  2: '0010',
-  3: '0011',
-  4: '0100',
-  5: '0101',
-  6: '0110',
-  7: '0111',
-  8: '1000',
-  9: '1001'
-}
+// The ^ operator returns a 1 when the bits are difference 
 
-
-const hammingDistance = (x, y) => {
-  const xBits = convertToBits(x);
-  const yBits = convertToBits(y);
-
-  console.log(xBits)
-  console.log(yBits)
-
-  let differenceCount = 0
-  for (let i = 0; i < xBits.length; i++) {
-    if (xBits[i] === undefined || yBits[i] === undefined) differenceCount += 4
-    else differenceCount += bitDifference(xBits[i], yBits[i])
-  }
-
-  return differenceCount
-}
-
-const convertToBits = (num) => {
-  return num.toString()
-     .split("")
-     .reduce((acc, char) => {
-       acc.push(bitHash[char]);
-       return acc;
-     }, [])
-}
-
-const bitDifference = (numOne, numTwo) => {    
-    let count = 0
-    
-    for (let i = 0; i < numOne.length; i++) {     
-      count += bitNumDifferenceCounter(numOne[i], numTwo[i])
-    }
-    return count                
-}
-
-
-const bitNumDifferenceCounter = (numOne, numTwo) => {
-  return (numOne !== numTwo) ? 1 : 0
-}
+const hammingDistance = function(x, y) {
+  return  oneDiff = (x^y).toString(2)
+                         .split("")
+                         .filter(el => el === '1')
+                         .length
+};
