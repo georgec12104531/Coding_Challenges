3e88619c5b7627bb2324f0f6711f636015b33bc2
George Charles Chang <31757304+georgec12104531@users.noreply.github.com>
2019-01-27 22:47:50 -0800

    Create searchBST.js

---
+++ searchBST.js
@@ -0,0 +1,10 @@
+
+const searchBST = (root, val) => {
+    if (!root || root.val === val ) {
+        return root;   
+    } else if (root.val > val) {
+        return searchBST(root.left, val);   
+    } else {
+        return searchBST(root.right, val);   
+    }
+};
