49a50406c625cd5ae8c131792084bcbd37a9e7e5
George Charles Chang <31757304+georgec12104531@users.noreply.github.com>
2019-02-05 16:46:20 -0800

    Update middleNode.js

--- middleNode.js
+++ middleNode.js
@@ -24,3 +24,47 @@ const findMidNode = (head, magicNumber, counter) => {
     
     return findMidNode(head.next, magicNumber, counter + 1)
 }
+
+///
+
+var middleNode = function(head) {
+    let mid = findMidNode(head, 0);
+    while (mid > 0) {
+        head = head.next;
+        mid -= 1;
+    }
+    
+    return head
+};
+
+const findMidNode = (head, length) => {
+    let node = head
+    while (node) {
+        node = node.next
+        length += 1
+    }
+    
+    return isEven(length) ? Math.floor(length / 2) : Math.floor(length / 2)
+}
+
+const isEven = (num) => {
+  return num % 2 === 0
+}
+
+////
+
+const middleNode = (head) => {
+    let slow = head;
+    let fast = head;
+    
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    
+    return slow
+};
+
+
+
+
