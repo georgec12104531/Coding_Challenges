92a60f896b28f3862fd870a62b65097ebf21c68f
George Charles Chang <31757304+georgec12104531@users.noreply.github.com>
2019-02-17 18:50:40 -0800

    Update reverseLinkedList.js

--- reverseLinkedList.js
+++ reverseLinkedList.js
@@ -33,3 +33,72 @@ const reverseLinkedList = (head) => {
   head.next = null;
   return reverseHead;
 }
+
+function Node(value) {
+  this.value = value;
+  this.next = null;
+}
+
+
+//In action
+
+
+class LinkedList {
+  constructor(node) {
+    this.head = node;
+    this.length = 0;
+  }
+
+  add(val) {
+    let newNode = new Node(val)
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = newNode;
+  }
+
+  show() {
+    let node = this.head;
+  
+    while (node) {
+      console.log(node.value)
+      node = node.next
+    }
+  }
+}
+
+const firstNode = new Node(1);
+const newLinkedList = new LinkedList(firstNode)
+newLinkedList.add(2);
+newLinkedList.add(3);
+newLinkedList.add(4);
+newLinkedList.add(5);
+console.log(newLinkedList.show())
+
+console.log(newLinkedList.head, 'this is the head before the reversing')
+
+
+const reverse = (head) => {
+  if (!head || !head.next) {
+    return head;
+  }
+
+  let reversedHead = reverse(head.next);
+  head.next.next = head;
+  head.next = null;
+
+  return reversedHead
+}
+
+console.log(reverse(newLinkedList.head), 'this is the head after')
+
+
+
+
+
+
+
+
+
+
