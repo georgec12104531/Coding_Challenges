0024209ef8f171b2be5b58f197a9daf7053b6afe
George Charles Chang <31757304+georgec12104531@users.noreply.github.com>
2019-02-19 10:59:12 -0800

    Create findComplement.js

---
+++ findComplement.js
@@ -0,0 +1,8 @@
+// const findComplement = (num) => {
+//   const bit = (num).toString(2);
+//   const inverse = bit.split("")
+//                      .map(el => el === '1' ? '0' : '1')
+//                      .join("")
+
+//   return parseInt(inverse, 2)  
+// }
