75098cd18c90484056a7f7f4f1f223b04f92aa76
George Charles Chang <31757304+georgec12104531@users.noreply.github.com>
2019-02-24 12:06:09 -0800

    Annotate searchBST.js

--- searchBST.js
+++ searchBST.js
@@ -1,10 +1,17 @@
+Note:
+- You need to return each recursive searchBST call in order to actually return 
+the node in the end. Without it the function will just return undefined.
+
 
 const searchBST = (root, val) => {
+    //if the node if null or the value of the node is the value, return the node.
     if (!root || root.val === val ) {
         return root;   
     } else if (root.val > val) {
+    //if the node's value if greater than the target value, look on the left side of the BST
         return searchBST(root.left, val);   
     } else {
+    // if the node's value is less than the target value, look on the right side of the BST
         return searchBST(root.right, val);   
     }
 };
