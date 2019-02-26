f40771c5849a3f6c1a3236bad596d21b336d6c92
George Charles Chang <31757304+georgec12104531@users.noreply.github.com>
2019-02-26 09:46:01 -0800

    Update hasPathSum.js

--- hasPathSum.js
+++ hasPathSum.js
@@ -1,3 +1,5 @@
+//Tail End Recursion
+
 const hasPathSum = function(root, sum) {
     return hasPath(root, sum, 0);
 };
@@ -14,4 +16,23 @@ const hasPath = (root, sum, count) => {
     
     return hasPath(root.left, sum, count) || hasPath(root.right, sum, count)
 }
+
+
+
+//Closure Version
+const hasPathSum = function(root, sum) {
+    const hasPath = (root, sum, target) => {
+        if (!root) return false 
+        target += root.val
+        
+        if (!root.left && !root.right) {
+            return sum === target
+        }
+        
+        return hasPath(root.left, sum, target) || hasPath(root.right, sum, target)
+    }
+    return hasPath(root, sum, 0)
+};
+
+
 
