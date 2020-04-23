5a5781d353f393d723cad005c983db64fdc77a5a
George Charles Chang <31757304+georgec12104531@users.noreply.github.com>
2020-04-23 12:22:35 -0700

    Update postOrderTree.js

--- postOrderTree.js
+++ postOrderTree.js
@@ -1,15 +1,23 @@
-var postorder = function(root) {
-    return createPostOrder(root, [])
-};
-
-const createPostOrder = (node, list) => {
-    if (!node) return list;
-    
-    node.children.forEach(child => {
-        createPostOrder(child, list)
-    });
-    
-    list.push(node.val);
-    
-    return list;
+function Node(val, children) {
+  this.val = val;
+  this.children = children;
 }
+
+const five = new Node(5, []);
+const six = new Node(6, []);
+const three = new Node(3, [five, six]);
+const two = new Node(2, []);
+const four = new Node(4, []);
+const one = new Node(1, [three, two, four]);
+
+var postorder = function(root) { 
+    if(!root) return [];  
+    let resultArr = [];
+
+    root.children.forEach((child) => {
+      resultArr = resultArr.concat(postorder(child))
+    })
+    resultArr.push(root.val);
+
+    return resultArr;
+};
