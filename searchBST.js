7012ba662b7a6651f81d99db19ca7eb1a9468436
George Charles Chang <31757304+georgec12104531@users.noreply.github.com>
2020-04-25 03:32:27 -0700

    Update searchBST.js

--- searchBST.js
+++ searchBST.js
@@ -15,3 +15,46 @@ const searchBST = (root, val) => {
         return searchBST(root.right, val);   
     }
 };
+
+//
+
+function Node(val, left=null, right=null) {
+  this.val = val;
+  this.left = left;
+  this.right = right;
+}
+
+const one = new Node(1);
+const three = new Node(3);
+const two = new Node(2, one, three);
+const seven = new Node(7);
+const four = new Node(4, two, seven);
+
+// Plan:
+// - loop through the array 
+// - if !node return null;
+// - if the node.val = val return node;
+// - call searchBST on the left if the value is less than the current node's value
+// - call searchBST on the right if the values is greater than the current node's value
+
+
+const searchBST = (root, val) => {
+  if (!root) return [];
+  if (root.val === val) return root;
+
+  if (root.val > val && root.left) {
+    return searchBST(root.left, val);
+  }
+  if (root.val < val && root.right) {
+    return searchBST(root.right, val);
+  }
+}; 
+
+const searchBST = (root, val) => {
+  while (root) {
+    if (root.val === val) return root;
+    root = root.val > val ? root.left : root.right;
+  }
+}
+
+searchBST(four, 1)
