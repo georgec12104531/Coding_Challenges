1595bc27642f842be720f7bb849df9420382f1fb
George Charles Chang <31757304+georgec12104531@users.noreply.github.com>
2020-05-02 14:25:24 -0700

    Update longestPalindrome.js

--- longestPalindrome.js
+++ longestPalindrome.js
@@ -38,3 +38,52 @@ var longestPalindrome = function(s) {
 
 
 longestPalindrome('cbbd')
+
+
+// Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+// Example 1:
+
+// Input: "babad"
+// Output: "bab"
+// Note: "aba" is also a valid answer.
+// Example 2:
+
+// Input: "cbbd"
+// Output: "bb"
+
+// Plan: 
+// - Find the center of a palindrome 
+//   for loop
+// - assign left and right pointer to itself or the next element to the right;
+//   second for loop
+// - move left and right pointers as far out as element at left and element at right are the same 
+// - check to see that the length is greater than the longestLength 
+// - return longestLength 
+
+var longestPalindrome = function(s) {
+  let left = null, right = null;
+  let longestLength = 0;
+
+  for (let i = 0; i < s.length - 1; i++) {
+    for (let j = 0; j <= 1; j++) {
+      left = i;
+      right = i + j;
+
+      while(s[left] && s[left] === s[right]) {
+        left--;
+        right++;
+      }
+
+      length = right - left - 1;
+      
+      if (length > longestLength) {
+        longestLength = length; 
+      }
+    }
+  }
+  
+  return longestLength;
+}
+
+longestPalindrome('aaaaadddaaaaaaaasdfdsa');
