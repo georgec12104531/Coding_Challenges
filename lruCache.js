0e7e628b76577b857c199df060bd39742341ec20
George Charles Chang <31757304+georgec12104531@users.noreply.github.com>
2020-05-03 00:30:07 -0700

    Update LRU

--- lruCache.js
+++ lruCache.js
@@ -1,29 +1,87 @@
-function get(url) { 
-  const mostRecentPic = hashMap[url].photo;
-  hashMap[url].next = ;
-  hashMap[url].previous = ;
-  //Returns a photo attached to the URL
-  //O(1)
+
+// Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
+
+// get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+// put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+
+// The cache is initialized with a positive capacity.
+
+// Follow up:
+// Could you do both operations in O(1) time complexity?
+
+// Example:
+
+// LRUCache cache = new LRUCache( 2 /* capacity */ );
+
+// cache.put(1, 1);
+// cache.put(2, 2);
+// cache.get(1);       // returns 1
+// cache.put(3, 3);    // evicts key 2
+// cache.get(2);       // returns -1 (not found)
+// cache.put(4, 4);    // evicts key 1
+// cache.get(1);       // returns -1 (not found)
+// cache.get(3);       // returns 3
+
+function Node(val, next, prev) {
+  this.val = val;
+  this.next = next;
+  this.prev = prev;
 }
 
-function put(url, photo) {
-  //O(1)
+class LRU {
+  constructor(capacity) {
+    this.capacity = capacity;
+    this.size = 0;
+    this.cache = {};
+    this.head;
+    this.tail;
+
+  }
+
+  get(val) {
+    // console.log(this.cache);
+    return this.cache[val] ? this.cache[val].val : -1;
+  }
+
+  put(key, val) {
+    if (this.size === this.capacity) {
+      this.tail.prev = null;
+      this.tail.prev.next = null;
+      this.tail = tail.prev;
+    } else if (this.size === 0) {
+      
+    }
+    
+    const newNode = new Node(val, this.head, null);
+    if (newNode.next) {
+      newNode.next.prev = newNode;
+    }
+    console.log(newNode.next);
+    // newNode.next.prev = newNode;
+    this.head = newNode;
+    // this.head.next.prev = newNode;
+
+
+    this.cache[key] = newNode;
+    this.size++;
+  }
 }
 
+const lru = new LRU(2);
 
-LRU Cache 
-MAX_SIZE = 5; 
+lru.put(4, 4);
+lru.put(3, 3);
+// lru.put(2, 2);
 
- [1 2 3 4 5 ] 
- 
 
-node {
-  Photo, next, prev
-}
- 
- 
- {node} <-> {node} <-> {node}  <-> {node}
- 
- 
- - lruCache is hash containing a linked list
- ex. {url:node, url:node, ...}
+// lru.put(2, 2);
+// lru.put(1, 1);
+
+// cache.put(1, 1);
+// cache.put(2, 2);
+// cache.get(1);       // returns 1
+
+// console.log('-----------------------');
+// lru.get(3)?;
+
+// console.log(lru);
