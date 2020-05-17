cbabcec836cadd136f64254037e3be974e31127b
George Charles Chang <31757304+georgec12104531@users.noreply.github.com>
2020-05-16 23:05:54 -0700

    Update addTwoNumbers.js

--- add-two-numbers.js
+++ add-two-numbers.js
@@ -1,59 +1,42 @@
 function ListNode(val, next = null) {
-  this.val = val
+  this.val = val;
   this.next = next;
 }
 
-// LinkedList1
-const n3 = new ListNode(3);
-const n2 = new ListNode(2, n3);
-const n1 = new ListNode(1, n2);
+const three = new ListNode(3);
+const four = new ListNode(4, three);
+const two = new ListNode(2, four);
 
+const fourB = new ListNode(4);
+const six = new ListNode(6, fourB);
+const five = new ListNode(5, six);
 
-// LinkedList2
-const m3 = new ListNode(5);
-const m2 = new ListNode(4, m3);
-const m1 = new ListNode(3, m2);
-
-const joinArrOfStr = (arr) => {
-  return arr.join('');
-} 
-
-const returnReversedInteger = (current) => {
-  let reversedIntArray = new Array;
-
-  while (current) {
-    reversedIntArray.unshift(current.val)
-    current = current.next;
-  }
-
-  const strInt = joinArrOfStr(reversedIntArray);
-  return parseInt(strInt);
-}
+const five = new ListNode(5);
+const fiveB = new ListNode(5);
 
 const addTwoNumbers = (l1, l2) => {
-  const reversedIntOne = returnReversedInteger(l1);
-  const reversedIntTwo = returnReversedInteger(l2);
+  const dummyHead = new ListNode(null);
+  let prev = dummyHead;
+  let current;
+  let carryOver = 0;
 
-  return reversedIntOne + reversedIntTwo;
-};
-
-console.log(addTwoNumbers(n1, m1));
-
-const reverseLinkedList = (currentNode) => {
-  // keep current node
-  let previous = null;
-
-  // while the current node is truthy
-  while (currentNode) {
-    // Keep a copy of the next node 
-    let copyNextNode = currentNode.next;
-    // reverse the linked list, if first node point to null, if not then point to the previous one
-    currentNode.next = previous;
-    // make the currentNode the previous node
-    previous = currentNode;
-    // Move the current node forward
-    currentNode = copyNextNode; 
+  while(l1 || l2 || carryOver) {
+    let val1 = l1 ? l1.val : 0;
+    let val2 = l2 ? l2.val : 0;
+    let sum = val1 + val2 + carryOver;
+    if (sum >= 10) {
+      current = sum % 10;
+      carryOver = Math.floor(sum / 10);
+    } else {
+      current = sum;
+      carryOver = 0;
+    }
+    let newNode = new ListNode(current);
+    prev.next = newNode;
+    prev = newNode;
+    l1 = l1 ? l1.next : null;
+    l2 = l2 ? l2.next : null;
   }
 
-  return previous;
+  return dummyHead.next;
 }
