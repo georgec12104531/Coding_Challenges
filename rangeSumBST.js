c85936648aea7f0f0b8182188f0072a7ded09592
George Charles Chang <31757304+georgec12104531@users.noreply.github.com>
2020-06-02 12:14:50 -0700

    Update rangeSumBST.js

--- rangeSumBST.js
+++ rangeSumBST.js
@@ -1,37 +1,50 @@
-function Node(val, l, r) {
+// Plan
+// Initialize a sum = 0;
+// create a function addNumbers(root, sum, l, r)
+  // if (root.val <= R && root.val >= L) sum += root.val
+  // addNumber(root.left, sum, l, r);
+  //  
+// Input: root = [10,5,15,3,7,null,18], L = 7, R = 15
+// Output: 32
+
+function Node(val, left=null, right=null) {
   this.val = val;
-  this.left = l;
-  this.right = r;
+  this.left = left;
+  this.right = right;
 }
 
+  //    10 
+  //    / \ 
+  //   5   15
+  //  /\  /  \
+  // 3  7null 18
 
 
-const four = new Node(4, null, null);
-const eight = new Node(8, null, null);
-const seven = new Node(7, four, eight);
-const six = new Node(6, null, null);
-const thirteen = new Node(13, null, null);
-const eleven = new Node(11, six, thirteen);
-const ten = new Node(10, seven, eleven);
+const eighteen = new Node(18);
+const fifteen = new Node(15, null, eighteen);
 
+const seven = new Node(7);
+const three = new Node(3);
+const five = new Node(5, three, seven);
+
+const ten = new Node(10, five, fifteen);
 
 var rangeSumBST = function(root, L, R) {
-  if (!root.val) return 0;
   let sum = 0;
 
-  if (root.val >= L && root.val <= R) {
-    sum += root.val;
+  // rangeSumBst has closure over addNumbers, sum, L, R are accessible 
+  function addNumbers(root) {
+    if (!root) return;
+    if (root.val >= L && root.val <= R) {
+      sum += root.val;
+    }
+     
+    addNumbers(root.left);
+    addNumbers(root.right);
   }
 
-  if (root.left) {
-    sum += rangeSumBST(root.left, L, R);
-  } 
-
-  if (root.right) {
-    sum += rangeSumBST(root.right, L, R);
-  }
-
+  addNumbers(root);
   return sum;
 };
 
-rangeSumBST(ten, 6, 12);
+rangeSumBST(ten, 7, 15);
