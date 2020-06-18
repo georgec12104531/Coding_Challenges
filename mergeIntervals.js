4046dfce309eb89096a5bfdb873e49c2fa876239
George Charles Chang <31757304+georgec12104531@users.noreply.github.com>
2020-06-18 14:28:33 -0700

    Update mergeIntervals.js

--- mergeIntervals.js
+++ mergeIntervals.js
@@ -27,4 +27,51 @@ const mergeIntervals = (intervals) => {
   return result;
 }
 
+
+
+
+// --------------------
+
+// Input: [[1,3],[2,6],[8,10],[15,18]]
+// Output: [[1,6],[8,10],[15,18]]
+// Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+
+// Plan
+// Create merged 
+// Add first interval to merged
+// Sort the intervals - n(log(n))
+// Loop through the sorted intervals, starting with the interval at index 1
+  // set currentInt = merged[merged.length - 1] 
+  // if the start time current interval <= currentIn end time
+    // currentInt[endTime] = current end time
+  // else
+    // add the current into interval 
+    // reassign currentIn
+// return the merged 
+
+
+const mergeIntervals = (intervals) => {
+  if (!intervals.length) return [];
+  const sorted = intervals.sort((a, b) => a[0] - b[0])
+  let merged = [sorted[0]];
+
+  // console.log(merged)
+  
+  for(let i = 1; i < sorted.length; i++) {
+    let time = sorted[i];
+    let currentInt = merged[merged.length - 1];
+
+    if (time[0] <= currentInt[1]) {
+
+      currentInt[1] = Math.max(currentInt[1], time[1]);
+    } else {
+      merged.push(time);
+    }
+  }
+
+  return merged;
+}
+
+mergeIntervals([[1,4],[2,3]])
+
 mergeIntervals([[1,3],[2,6],[3,10],[15,18]])
