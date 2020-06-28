f82109232d1baa580c5fc87741c8b39c9acf1098
George Charles Chang <31757304+georgec12104531@users.noreply.github.com>
2020-06-28 13:18:44 -0700

    Create largestNum.js

---
+++ largestNumFromArr.js
@@ -0,0 +1,21 @@
+// Input: [3,30,34,5,9]
+// Output: "9534330"
+
+// Plan:
+// .sort the nums
+// compare the digits
+  // we need both numbers to have the same amount of digits 
+  // concating the numbers together, and then again in reverse order 
+  // concat the digits together for the first number 
+  // concat reverse for the second digit ex. `${b}${a}` - `${a}${b}`
+  //
+// compare the numbers
+// return arr.join('')
+
+const largestNum = (nums) => {
+  return nums.sort((a, b) => {
+    return `${b}${a}` - `${a}${b}`
+  }).join("")
+}
+
+largestNum([3,30,34,5,9])
